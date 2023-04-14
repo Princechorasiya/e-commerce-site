b3541c78ef06366eb051c6b71730a19ed1e7ce1d
Princechorasiya <princechorasiya6@gmail.com>
2023-04-14 19:24:12 +0530

    home

---
+++ src/pages/homepage/Hompage.Component.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./hompage.scss"
+import Directory from './../../components/directory/Directory.component';
+
+
+const HomePage = () => {
+  return (
+    <>
+      <div className="homepage">
+<Directory/>
+      </div>
+
+    </>
+  )
+}
+
+export default HomePage
